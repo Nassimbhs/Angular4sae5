b275d09794d7a9ce6e1badfa1ac805677de2efaf
[your <benhassinenassim98@gmail.com>
2022-04-20 12:21:58 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
-import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { CarComponent } from './car/car.component';
+import { ChatComponent } from './chat/chat.component';
+import { TripComponent } from './trip/trip.component';
+import { InvitationComponent } from './invitation/invitation.component';
+import { ForumComponent } from './forum/forum.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+
+const routes:Routes = [
+  { path: 'cars' , component:CarComponent },
+  { path: 'chat' , component:ChatComponent },
+  { path: 'profile' , component:ProfileComponent },
+  { path: 'trip' , component:TripComponent },
+  { path: 'feedback' , component:FeedbackComponent },
+  { path: 'forum' , component:ForumComponent },
+  { path: 'invitation' , component:InvitationComponent },
+
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    ContentComponent,
+    CarComponent,
+    ChatComponent,
+    TripComponent,
+    InvitationComponent,
+    ForumComponent,
+    ProfileComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
