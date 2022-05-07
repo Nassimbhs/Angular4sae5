e36462470dd6d33bc03ef5f7215b68fd431e6903
Hana-jenhani <Hana.janhani@esprit.tn>
2022-05-07 19:46:26 +0200

    update+deletetravel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListtravelComponent } from './travels/listtravel/listtravel.component'; 
 import { DetailtravelComponent } from './travels/detailtravel/detailtravel.component';
 import {AddtravelComponent} from './travels/addtravel/addtravel.component';
-
+import { UpdatetravelComponent } from './travels/updatetravel/updatetravel.component';
 const routes: Routes = [
   
    { path: "add-travel", component : AddtravelComponent},
  
    { path: "list-travel", component  : ListtravelComponent },
    { path: "detailtravel/:id", component : DetailtravelComponent  },
-
+   { path: "update-travel/:id", component : UpdatetravelComponent},
    { path: "", redirectTo : "travels", pathMatch:"full"} 
 ];
 
--- src/app/travels/updatetravel/updatetravel.component.ts
+++ src/app/travels/updatetravel/updatetravel.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TravelModel } from 'src/app/modal/TravelModel'; 
+import { travelService } from 'src/app/service/travelService';
 
 @Component({
   selector: 'app-updatetravel',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./updatetravel.component.css']
 })
 export class UpdatetravelComponent implements OnInit {
+  id: any;
+  travel: TravelModel;
 
-  constructor() { }
-
+constructor(private router: Router,private cs: travelService, private ac: ActivatedRoute)
+{}
+  
+  
   ngOnInit(): void {
+  this.ac.params.subscribe(params=>{this.id=params['id']});
+  this.updateTravelByid(this.id);
+  
+  }
+  updateTravelByid(id){
+    this.cs.updateTravelByid(id).subscribe(res=>{ 
+      console.log(res);
+      this.travel=res});
   }
 
+  updateTravel() {
+    this.cs.updateTravel(this.travel).subscribe(()=>this.router.navigateByUrl("/list-travel"));
+    }
 }
